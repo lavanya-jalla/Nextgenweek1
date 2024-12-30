8ac4bbfef80b9c13c5ba139bb1557132ec62bfa9
Jalla Lavanya <lavanya123jalla@gmail.com>
2024-12-30 21:01:02 +0530

    Your commit message describing the changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,9 @@ const NavBar = () => {
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                
                 <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
             </ul>
         </nav>
     );
